72b23e99e707ed881d557be119620f9aa7111ca5
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2023-07-29 19:56:49 +0200

    v1.0.0

--- src/_router/PublicRouter.jsx
+++ src/_router/PublicRouter.jsx
@@ -2,18 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "../pages/Layout";
 import Home from "../pages/Home/Home";
 import EmployeeList from "../pages/EmployeeList/EmployeeList";
-import Error from "../pages/Error";
+//import Error from "../pages/Error";
 
 const PublicRouter = () => {
   return (
     <Routes>
-      <Route element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
 
         <Route path="/home" element={<Home />} />
         <Route path="/employee-list" element={<EmployeeList />} />
 
-        <Route path="*" element={<Error />} />
+        <Route path="*" element={<Home />} />
       </Route>
     </Routes>
   );
